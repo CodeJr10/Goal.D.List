4a10cff0cc68a561f5ab294c6d640d6e9c8a0506
CodeJr10 <aadityamalap666@gmail.com>
2026-05-05 15:58:06 +0530

    feat: task creation functionality added

--- script.js
+++ script.js
@@ -20,15 +20,15 @@ const closeSidebar = () => {
 hamburgerBtn.addEventListener("click", openSidebar);
 closeBtn.addEventListener("click", closeSidebar);
 
-// Task Creation Functionality
+// Task Modal Functionality
 const taskBtn = document.getElementsByClassName("add-task")[0];
 const taskModal = document.getElementById("taskModal");
 const overlayEffect = document.getElementsByClassName("overlay")[0];
 const closeModalBtn = document.getElementsByClassName("closeModal")[0];
 
-console.log({ taskBtn, taskModal, overlayEffect, closeModalBtn });
+// console.log({ taskBtn, taskModal, overlayEffect, closeModalBtn });
 
-const createTask = () => {
+const openTaskModal = () => {
   taskModal.classList.toggle("active");
   overlayEffect.classList.toggle("active");
 };
@@ -38,9 +38,44 @@ const closeTaskModal = () => {
   overlayEffect.classList.remove("active");
 };
 
-taskBtn.addEventListener("click", createTask);
+taskBtn.addEventListener("click", openTaskModal);
 closeModalBtn.addEventListener("click", closeTaskModal);
 
+// Task Creation Functionality
+const createTaskBtn = document.getElementById("add-task-btn"); // create task button inside the modal
+const getTaskTitle = document.getElementById("task-Title");
+const getTaskDescription = document.getElementById("task-Description");
+const getTaskDate = document.getElementById("task-Date");
+
+const createNewTask = () => {
+  const newDiv = document.createElement("div");
+  newDiv.classList.add("task-card");
+
+  const taskTitle = document.createElement("h3");
+  taskTitle.classList.add("task-title");
+  taskTitle.textContent = getTaskTitle.value;
+  newDiv.appendChild(taskTitle);
+
+  const taskDescription = document.createElement("p");
+  taskDescription.classList.add("task-Description");
+  taskDescription.textContent = getTaskDescription.value;
+  newDiv.appendChild(taskDescription);
+
+  const taskDate = document.createElement("h5");
+  taskDate.textContent = getTaskDate.value;
+  newDiv.appendChild(taskDate);
+
+  const taskContainer = document.getElementsByClassName("task-list")[0];
+  taskContainer.appendChild(newDiv);
+
+  // Clear input fields and close modal
+  getTaskTitle.value = "";
+  getTaskDescription.value = "";
+  getTaskDate.value = "";
+  closeTaskModal();
+};
+
+createTaskBtn.addEventListener("click", createNewTask);
 // Date Display Functionality
 const dateEL = document.getElementById("todoDate");
 
